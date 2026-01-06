c78c7fdd90b67c1f72981ed9cb60b2bcbd59ce19
Sherry <sherry.hsieh@cardinalblue.com>
2026-01-06 21:50:29 +0800

    feat: firebase setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@vueuse/nuxt", "nuxt-icon", "nuxt-vuefire"],
+  typescript: { shim: false },
+
+  vuefire: {
+    auth: {enabled: true, sessionCookie: true},
+    config: {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId,
+      appId: process.env.appId,
+      // there could be other properties depending on the project
+    },
+  },
+});
